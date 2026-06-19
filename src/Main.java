b298363683fc8471b5557597f81a292e17c8e975
tooderjinyoung <iac1393too@gmail.com>
2026-06-19 22:55:50 +0900

    Delete src directory

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-
-    System.out.println("삼성전자 화이팅");
-    System.out.println("하이닉스 화이팅");
-}
